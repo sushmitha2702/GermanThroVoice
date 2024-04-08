bfe0f530684452c376df4298d26e346e1acd91a6
Sushmitha <sushmitha.s.96@gmail.com>
2024-04-09 00:40:20 +0200

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import NounView from '../views/NounView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'noun',
+      component: NounView
+    },
+    {
+      path: '/verb',
+      name: 'verb',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/VerbView.vue')
+    },
+    {
+      path: '/simple-sentence',
+      name: 'simple-sentence',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SimpleSentenceView.vue')
+    }
+  ]
+})
+
+export default router

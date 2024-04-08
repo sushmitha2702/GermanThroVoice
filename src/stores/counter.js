794bc5cdb38d412418dd4fda9692e341262d7a48
Sushmitha <sushmitha.s.96@gmail.com>
2024-04-09 00:40:58 +0200

    Create counter.js

---
+++ src/stores/counter.js
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const noun_min_count = ref(0)
+  const noun_count_per_call = ref(10)
+  const verb_min_count = ref(0)
+  const verb_count_per_call = ref(10)
+  const sentence_count = ref(0)
+  function noun_max_count() {
+    return noun_min_count.value + noun_count_per_call.value
+  }
+
+  return { noun_min_count, noun_count_per_call, verb_min_count, verb_count_per_call, sentence_count, noun_max_count }
+})
